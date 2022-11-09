42742c0c48fb19bc47467d020427ae02f5ef13bf
MacNCheze <112411744+MacNCheze@users.noreply.github.com>
2022-11-09 11:56:16 -0500

    update proj 11/9/22

---
+++ app/src/main/java/edu/farmingdale/alrajab/week12_auth_ml_api_demo/GoogleSignInActivity.kt
@@ -0,0 +1,14 @@
+package edu.farmingdale.alrajab.week12_auth_ml_api_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleSignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_sign_in)
+
+
+
+    }
+}
