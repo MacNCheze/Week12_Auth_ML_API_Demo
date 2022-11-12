d1d194e80031295ffaf80019707d007b0a541554
MacNCheze <krauthr87@gmail.com>
2022-11-12 15:17:41 -0500

    11/12/22 changes2

--- app/src/main/java/edu/farmingdale/alrajab/week12_auth_ml_api_demo/LandingActivity.kt
+++ app/src/main/java/edu/farmingdale/alrajab/week12_auth_ml_api_demo/LandingActivity.kt
@@ -1,24 +1,44 @@
 package edu.farmingdale.alrajab.week12_auth_ml_api_demo
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import com.google.firebase.auth.FirebaseAuth
 import edu.farmingdale.alrajab.week12_auth_ml_api_demo.databinding.ActivityLandingBinding
 
 class LandingActivity : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     lateinit var binding: ActivityLandingBinding
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    private val pickImage = 100
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLandingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.logoutBtn.setOnClickListener { logout() }
-
-
         firebaseAuth = FirebaseAuth.getInstance()
 
+
+        imageView = findViewById(R.id.imageView)
+        button = findViewById(R.id.load_iamge_btn)
+        button.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            imageView.setImageURI(imageUri)
+        }
     }
 
     private fun logout() {
